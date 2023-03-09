c03fb5a134b482f2e92a586515777c89fbe86c2f
Abdulkadir Copur <kadir_0964@hotmail.com>
2023-03-09 16:30:16 +0300

    Last version added

--- PerceptronModel/Program.cs
+++ PerceptronModel/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace PerceptronModel
 {
@@ -13,74 +14,128 @@ namespace PerceptronModel
         }
     }
 
-
     class Program
     {
         static void Main(string[] args)
         {
-            Random r = new Random();
-            
-            double[] x1Inputs = {0.6, 0.2, -0.3, -0.1, 0.1, -0.2, -0.4, -0.6};
-            double[] x2Inputs = { 0.5, 0.4, -0.5, -0.1, 0.1, 0.7, -0.2, -0.3 };
-            double[] weights = new double[x1Inputs.Length];
-           
-            for (int i = 0; i < x1Inputs.Length; i++)
-            {
-                weights[i] = r.NextDouble(-1, 1);
-            }
+            double[,] dataSet = { { 0.6, 0.5, 1 }, { 0.2, 0.4, 1 }, { -0.3, -0.5, -1 }, { -0.1, -0.1, -1 }, { 0.1, 0.1, 1 }, { -0.2, 0.7, 1 }, { -0.4, -0.2, -1 }, { -0.6, 0.3, -1 } };
+            Neuron neuron = new Neuron(dataSet);
 
+            neuron.learn(10);
+            Console.WriteLine("Accuracy Rate After 10 epoch Supervised Learning: %{0}", neuron.calculateAccRate() * 100);
+            neuron.learn(90);
+            Console.WriteLine("Accuracy Rate After 100 epoch Supervised Learning: %{0}", neuron.calculateAccRate() * 100);
+
+            // Test Datas
+            double[,] testDataSet_1 = { { 0.6, 0.4, 1 }, { -0.7, 0.3, -1 }, { 0.9, 0.2, 1 }, { 0.4, -0.6, -1 }, { 0.5, 0.1, 1 }, { -0.8, -0.3, -1 }, { -0.2, 0.7, 1 }, { 0.8, 0.3, 1 } };
+            double[,] testDataSet_2 = { { 0.2, -0.1, 1 }, { -0.5, 0.6, 1 }, { 0.6, -0.1, 1 }, { 0.2, -0.8, -1 }, { 0.1, -0.3, -1 }, { -0.4, 0.1, -1 }, { 0.6, -0.3, 1 }, { -0.5, 0.1, -1 } };
+            double[,] testDataSet_3 = { { 0.9, -0.3, 1 }, { 0.4, -0.6, -1 }, { 0.7, 0.3, 1 }, { 0.8, -0.2, 1 }, { -0.5, 0.4, -1 }, { 0.1, 0.3, 1 }, { 0.5, -0.4, 1 }, { 0.7, -0.6, 1 } };
+            double[,] testDataSet_4 = { { -0.1, 0.2, 1 }, { 0.7, -0.9, -1 }, { -0.2, -0.6, -1 }, { 0.8, 0.6, 1 }, { 0.4, 0.8, 1 }, { -0.4, -0.5, -1 }, { -0.3, -0.2, -1 }, { -0.2, 0.6, 1 } };
+            double[,] testDataSet_5 = { { 0.3, 0.7, 1 }, { 0.1, -0.8, -1 }, { 0.8, -0.2, 1 }, { -0.7, -0.4, -1 }, { -0.6, -0.4, -1 }, { 0.7, 0.2, 1 }, { -0.7, -0.1, -1 }, { 0.6, 0.8, 1 } };
+
+            List<double[,]> testDatas = new List<double[,]>();
+            testDatas.Add(testDataSet_1);
+            testDatas.Add(testDataSet_2);
+            testDatas.Add(testDataSet_3);
+            testDatas.Add(testDataSet_4);
+            testDatas.Add(testDataSet_5);
+
+            Console.WriteLine("\nAccuracy Rates For Test Datas");
+            Console.WriteLine("-----------------------------");
+
+            for (int i = 0; i < testDatas.Count; i++)
+            {
+                neuron.dataSet = testDatas[i];
+                Console.WriteLine("Accuracy rate for testData-{0}: %{1}", i + 1, neuron.calculateAccRate() * 100);
+            }
         }
     }
 
     class Neuron
     {
-        public double[] x1Inputs;
-        public double[] x2Inputs;
-        public double[] weights;
+        const double lambda = 0.05; // learning coefficient
+        static Random r = new Random();
 
-        
-        
-        public Neuron(double[] x1Inputs, double[] x2Inputs, double[] weights)
-        {
-            this.x1Inputs = x1Inputs;
-            this.x2Inputs = x2Inputs;
-            this.weights = weights;
-        }
-        
-        public int[] calculateOutput()
-        {
-            int[] outputs = new int[weights.Length]; 
-            for (int i = 0; i < x1Inputs.Length; i++)
-            {
-                if ((x1Inputs[i] * weights[i]) + (x2Inputs[i] * weights[i]) < 0)
-                {
-                    outputs[i] = -1;
-                }
-                else
-                {
-                    outputs[i] = 1;
-                }
-            }
+        public double[,] dataSet;
+        public double[] weightSet;
 
-            return outputs;
+        public Neuron(double[,] dataSet)
+        {
+            this.dataSet = dataSet;
+            weightSet = generateRandomWeights();
         }
 
-        public void editWeights()
+        // Generates random 2 weights' values for inputs
+        private double[] generateRandomWeights()
         {
-            int[] outputs = calculateOutput();
-            for (int i = 0; i < x1Inputs.Length; i++)
+            double[] randomWeights = new double[dataSet.GetLength(1) - 1];
+
+            for (int i = 0; i < randomWeights.GetLength(0); i++)
             {
-                if (((x1Inputs[i]  + (x2Inputs[i]) < 0) && outputs[i] == 1))
-                {
-                    
-                }
+                randomWeights[i] = r.NextDouble(-1, 1);
+            }
+            return randomWeights;
+        }
 
-                if (((x1Inputs[i] + (x2Inputs[i]) > 0) && outputs[i] == -1))
-                {
+        // Calculates the output value of inputs
+        private int calculateOutput(double[] data)
+        {
+            if ((data[0] * weightSet[0]) + (data[1] * weightSet[1]) < 0)
+            {
+                return -1;
+            }
+            return 1;
+        }
 
+        // Changes the weights' values if target and output are not equal
+        private void editWeights(double[] data)
+        {
+            if (data[2] < 0 && calculateOutput(data) == 1)
+            {
+                weightSet[0] += lambda * -2 * data[0];
+                weightSet[1] += lambda * -2 * data[1];
+            }
+            else if (data[2] > 0 && calculateOutput(data) == -1)
+            {
+                weightSet[0] += lambda * 2 * data[0];
+                weightSet[1] += lambda * 2 * data[1];
+            }
+        }
+
+        // Edits weights according to input values and it does that until epoch number is reached
+        public void learn(int epokNumber)
+        {
+            for (int j = 0; j < epokNumber; j++)
+            {
+                for (int i = 0; i < dataSet.GetLength(0); i++)
+                {
+                    double[] data = new double[dataSet.GetLength(1)];
+                    data[0] = dataSet[i, 0]; data[1] = dataSet[i, 1]; data[2] = dataSet[i, 2];
+
+                    editWeights(data);
                 }
             }
+        }
+
+        // Finds the accuracy rate 
+        public double calculateAccRate()
+        {
+            double dataLength = dataSet.GetLength(0);
+            double accNum = 0;
+
+            for (int i = 0; i < dataSet.GetLength(0); i++)
+            {
+                double[] data = new double[dataSet.GetLength(1)];
+                data[0] = dataSet[i, 0]; data[1] = dataSet[i, 1]; data[2] = dataSet[i, 2];
+
+                if (((dataSet[i, 2] < 0) && calculateOutput(data) == -1) || ((dataSet[i, 2] > 0) && calculateOutput(data) == 1))
+                {
+                    accNum++;
+                }
+            }
+            return (accNum / dataLength);
         }
     }
 }
+
 
